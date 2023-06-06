6e93270d54c467709ee24ec3c374d2bff8ddd6c7
Adrien <azagar@kaliop.com>
2023-06-06 15:08:25 +0200

    changing sw file

--- push.js
+++ push.js
@@ -7,7 +7,14 @@ const vapidKeys = {
 
 push.setVapidDetails()
 
-let sub = {};
+let sub = {
+  endpoint:"https://fcm.googleapis.com/fcm/send/dYW6xynrDFI:APA91bEUDGIsIPxrhX_3yPjpW-4SZTMzjDBpwsTFpzEN7luilyLvkxDFE9CmGzrTvzSENbaEHFyntJa4569viLrJsJ3o2nUZCoSqoi1MckTx32vx3lb1mc996t8v89q_SIJPmpztyOv_",
+  "expirationTime":null,
+  "keys":{
+    "p256dh":"BJgNmz3cEe8rgxJ587QKzLFI-D2JRavfvLFFutaV-CwnPSa8ufX-RDYNN6wf2tUbdLA7Rs9YMccCbFO5eB8RNq4",
+    "auth":"UOhYteSL7HwtrkzFy10cPw"
+  }
+}
 
 push.sendNotification(sub, 'Hello world')
 
--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,17 @@
-self.addEventListener('push', () => {
-  self.registration.sendNotification('Hello world', {})
+self.addEventListener('push', (e) => {
+  let options = {
+    body: 'This notification was generated from a push',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: '2'
+    },
+    action: [
+      {action: 'explort', title: 'Explore PWA world'},
+      {action: 'close', title: 'close',}
+    ]
+  };
+  e.waitUntil(
+    self.registration.showNotification('Hellow PWA', options)
+  )
 })
