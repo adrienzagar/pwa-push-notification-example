a4418f5e32066e52f84dd16b084c3e77ca4a11ad
Adrien <azagar@kaliop.com>
2023-06-08 00:17:58 +0200

    pokedex

--- sw.js
+++ sw.js
@@ -19,9 +19,6 @@ for (let i = 1; i <= 10; i++) {
   urlsToCache.push(url);
 }
 
-console.log(urlsToCache)
-// debugger
-
 self.addEventListener('install', (e) => {
   console.log('[Service Worker] Install');
   e.waitUntil((async () => {
@@ -31,19 +28,6 @@ self.addEventListener('install', (e) => {
     await cache.addAll(urlsToCache);
   })());
 });
-
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request).then(function(response) {
-//       if (response) {
-//         return response;
-//       } else {
-//         return fetch(event.request);
-//       }
-//     })
-//   );
-// });
-
 
 self.addEventListener('fetch', (e) => {
   console.log(e)
@@ -56,7 +40,6 @@ self.addEventListener('fetch', (e) => {
 
 e.respondWith((async () => {
   const r = await caches.match(e.request);
-  console.log(r)
   console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
   if (r) return r;
   const response = await fetch(e.request);
@@ -67,28 +50,6 @@ e.respondWith((async () => {
 })());
 });
 
-// self.addEventListener('activate', function(event) {
-//   // ...
-
-//   // Ajoutez le code suivant pour activer le service worker lorsque vous êtes hors ligne
-//   if (navigator.onLine) {
-//     // Si le navigateur est en ligne, supprimez les anciens caches
-//     event.waitUntil(
-//       caches.keys().then(function(cacheNames) {
-//         return Promise.all(
-//           cacheNames.filter(function(cacheName) {
-//             return cacheName !== CACHE_NAME;
-//           }).map(function(cacheName) {
-//             return caches.delete(cacheName);
-//           })
-//         );
-//       })
-//     );
-//   } else {
-//     // Si le navigateur est hors ligne, activez immédiatement le service worker
-//     return self.clients.claim();
-//   }
-// });
 
 
 
